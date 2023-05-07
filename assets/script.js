bf845ede622220b217143871a2987472dd022867
Rafael Pereira <rafaelpersan@gmail.com>
2023-05-07 10:13:47 -0300

    Arryas

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+//Desconstruindo array
+
+let list = ['Rafael Pereira', 'Rafael', 'Pereira', '@rafaelpersan'];
+
+let [nomeCompleto, , , rede ] = list;
+
+console.log(nomeCompleto, rede);
+
+
+//Arrow functions 
